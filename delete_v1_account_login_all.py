0c0973817d2011b4d1732381d8578474acfcb9d8
DiLysenk <46103863+DiLysenk@users.noreply.github.com>
2023-08-17 13:19:21 +0300

    Update delete_v1_account_login_all.py
    deted payload

--- delete_v1_account_login_all.py
+++ delete_v1_account_login_all.py
@@ -8,7 +8,6 @@ def delete_v1_account_login_all():
     """
     url = "http://5.63.153.31:5051/v1/account/login/all"
 
-    payload = {}
     headers = {
         'X-Dm-Auth-Token': 'IQJh+zgzF5DL+x6PqzFdX34Z+5M8mulgP5ABi3OaCOYk+Pog0kQxnAdhA2dxLo0691oj1mFZYBTrzsyQsf5gga4so7MV8ezvh0HN87pPF6HlQe3SPa6MRUJQtCzejYc0UKhD4Kk/sj0=',
         'X-Dm-Bb-Render-Mode': '',
@@ -19,6 +18,5 @@ def delete_v1_account_login_all():
         method="DELETE",
         url=url,
         headers=headers,
-        json=payload
     )
     return response
