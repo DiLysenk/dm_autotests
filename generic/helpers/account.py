a907061ab0e10fbb7d20bf42275fa039ec1682b0
dmitriy.lysenko <dmitriy.lysenko@gs-labs.ru>
2023-09-24 23:10:15 +0300

    feat: added fixtures

--- generic/helpers/account.py
+++ generic/helpers/account.py
@@ -12,9 +12,9 @@ class Account:
     def set_headers(self, headers):
         self.facade.account_api.session.headers.update(headers)
 
-    def register_new_user(self, login: str, email: str, password: str):
-        # проверки зашиты
+    def register_new_user(self, login: str, email: str, password: str, status_code: int = 201):
         response = self.facade.account_api.post_v1_account(
+            status_code=status_code,
             json=Registration(
                 login=login,
                 email=email,
